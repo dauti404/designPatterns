c166323e79504a59587328e1ffb3225a91a137b3
dauti <augusto09frazao@gmail.com>
2025-11-25 11:44:25 -0300

    Código de geração de árvore comentado

--- AbstractFactory/code02/Application.java
+++ AbstractFactory/code02/Application.java
@@ -1,39 +1,60 @@
 class Application {
+
     // Atributos da classe
     // Composição do produto abstrato - variável que guarda o produto concreto
     private Product product;
-    private int eixoX = 0, eixoY = 0, elementK = 0;
-    private int altura = 16, copa = 0;
-    
+    // O tamanhoCopa define a Largura da copa da árvore
+    private int eixoX = 0,
+        eixoY = 0,
+        tamanhoCopa = 0;
+    private int direcaoX = 16,
+        direcaoCopa = 0;
+
     // Construct com composição da fábrica abstrata
     public Application(FactoryAbstract factory) {
-        // Criando o produto concreto e armazenando 
+        // Criando o produto concreto e armazenando
         product = factory.createProductConcrete();
     }
-    
+
     // Mostra o produto concreto para o cliente
-    public void productUI(){
-        for(elementK = 4; elementK <= 8; elementK++) {
-            for(eixoX = 1; eixoX < elementK; eixoX++) {
-                for(eixoY = altura; eixoY > eixoX; eixoY--) {
+    public void productUI() {
+        // Define o tamanho da copa da árvore
+        // O primeiro condição cria as copas e subcopas
+        // O segundo a largura da copa - recomendável os números serem pares ou ímpares
+        for (tamanhoCopa = 4; tamanhoCopa <= 8; tamanhoCopa++) {
+            // Define a altura da copa de acordo com o eixoX
+            for (eixoX = 1; eixoX < tamanhoCopa; eixoX++) {
+                // Define o local da copa da árvore
+                for (eixoY = direcaoX; eixoY > eixoX; eixoY--) {
+                    // Gera os espaços do desenho da árvore
                     System.out.print(" ");
                 }
-                for(eixoY = 0; eixoY < eixoX + copa; eixoY++){
+                // Cria a copa da árvore e define o local da copa da árvore
+                for (eixoY = 0; eixoY < eixoX + direcaoCopa; eixoY++) {
                     System.out.print("❇️ ");
                 }
+                // Quebra de linha
                 System.out.println(" ");
             }
-            altura--;
-            copa++;
+            // Direção da árvore no eixo X
+            direcaoX--;
+            // Largura da copa
+            direcaoCopa++;
         }
-        
-        for (eixoX = 1; eixoX < 9; eixoX++) {
-            for(eixoY = altura + 1; eixoY > 0; eixoY--) {
+
+        // Cria o tronco da árvore
+        // O primeiro comando - estabiliza o tamanho do tronco
+        // O segundo comando - a lagura do tronco
+        for (eixoX = 1; eixoX < 8; eixoX++) {
+            // Direção do tronco no eixo X
+            for (eixoY = direcaoX + 1; eixoY > 0; eixoY--) {
                 System.out.print(" ");
             }
-            for(eixoY = 0; eixoY < 4; eixoY++) {
+            // Largura do tronco no eixo X
+            for (eixoY = 0; eixoY < 4; eixoY++) {
                 System.out.print("🪵");
             }
+            // Quebra de linha
             System.out.println(" ");
         }
     }
